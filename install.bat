ae182047aaea6850bd92309f052786ce88b71c3e
wuw-shz <wuw62481@gmail.com>
2024-06-24 16:57:53 +0700

    @wuw.sh

--- install.bat
+++ install.bat
@@ -1,6 +1,2 @@
 @echo off
-setlocal
-
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/wuw-shz/MCBE-Scripting-Setup/powershell/install.ps1' -UseBasicParsing | Select-Object -ExpandProperty Content | Invoke-Expression"
-
-endlocal
